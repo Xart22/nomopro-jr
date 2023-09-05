8a225b5d20f5e656f8933fe365f4f1aee85024aa
Xart22 <fajar.rn22@gmail.com>
2023-09-05 09:13:38 +0700

    done

--- src/lib/layout-constants.js
+++ src/lib/layout-constants.js
@@ -56,8 +56,8 @@ STAGE_DISPLAY_SCALES[STAGE_DISPLAY_SIZES.largeConstrained] = 0.85; // large mode
 STAGE_DISPLAY_SCALES[STAGE_DISPLAY_SIZES.small] = 0.5; // small mode, regardless of browser size
 
 export default {
-    standardStageWidth: 600,
-    standardStageHeight: 420,
+    standardStageWidth: 480,
+    standardStageHeight: 360,
     fullSizeMinWidth: 1096,
     fullSizePaintMinWidth: 1250,
 };
